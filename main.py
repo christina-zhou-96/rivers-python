c97a5d1919b7b4484b1f5c58ff51b625dfcfd519
Christina Zhou <christina.zhou.96@gmail.com>
2023-04-04 14:40:17 -0400

    add unscaled

--- main.py
+++ main.py
@@ -36,116 +36,36 @@ knn_distances = pd.DataFrame(knn_distances)
 knn_distances['knn_score'] = knn_distances.mean(axis=1)
 knn_distances = pd.DataFrame(knn_distances['knn_score'])
 
-algo_df = algo_df.join(knn_distances)
+algo_df_with_score = algo_df.join(knn_distances)
 
-knn_anomalies = algo_df.nlargest(20, 'knn_score')
+knn_top_anomalies = algo_df.nlargest(20, 'knn_score')
+
+#### Try the above again but this time scale the dataset
 
 from sklearn.preprocessing import scale
 
-scale(algo_df)
-# KNNs
+scaled_df = pd.DataFrame(scale(algo_df))
 
-from pyod.pyod.models.knn import KNN # Strange import issue, so I had to go down two subdirectories
-import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+neigh2 = NearestNeighbors(n_neighbors=20)
+neigh2.fit(scaled_df)
 
-import numpy as np
+kneighbors2 = neigh2.kneighbors()
+knn_distances2 = kneighbors2[0]
+knn_indices2 = kneighbors2[1]
 
+# Let's build a KNN score for each point by taking the average distance from all its 20 neighbors
+knn_distances2 = pd.DataFrame(knn_distances2)
+knn_distances2['knn_score'] = knn_distances2.mean(axis=1)
+knn_distances2 = pd.DataFrame(knn_distances2['knn_score'])
 
+scaled_df = scaled_df.join(knn_distances2)
 
-# Tune the number of neighbors and contamination
-knn = KNN(n_neighbors=20, contamination=0.01, n_jobs=-1)
-# Let's find outliers
-knn.fit(algo_df)
-probs = knn.predict_proba(algo_df)
-# Use 55% threshold for filtering
-is_outlier = probs[:, 1] > 0.55
-# Isolate the outliers
-outliers = algo_df[is_outlier]
-len(outliers)
+knn_top_anomalies2 = scaled_df.nlargest(20, 'knn_score')
+
+# Scaling and unscaled generates different results
 
 
 
-# Quantile transformer
-from sklearn.preprocessing import QuantileTransformer
-
-qt = QuantileTransformer(output_distribution='normal')
-# What is the feature (independent variable) and what is the response (dependent variable?)
-# The feature or the cause is the temperature. The response or the effect is the nitrate levels.
-X = algo_df.drop('nitrate_mg_per_liter', axis=1)
-y = algo_df['nitrate_mg_per_liter']
-
-X.loc[:,:] = qt.fit_transform(X)
-
-# Fit a knn model on the scaled data
-knn_scaled = KNN(n_neighbors=20, contamination=0.01, n_jobs=-1)
-knn_scaled.fit(X, y)
-
-
-# load the data, split off the target
-cancer = load_breast_cancer(as_frame=True)
-cancer_df = cancer.frame
-cancer_features = cancer_df.drop(columns='target')
-# Split the data into training and test set
-X_train, X_test, y_train, y_test = train_test_split(cancer_features, cancer_df.target, random_state=23)
-
-# For comparison, run KNN again without scaling:
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train, y_train)
-print("accuracy without scaling: ", knn.score(X_test, y_test))
-
-# Scale the training data using StandardScaler
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-# Fit a knn model on the scaled data
-knn_scaled = KNeighborsClassifier(n_neighbors=5)
-knn_scaled.fit(X_train_scaled, y_train)
-
-# Scale the test data
-X_test_scaled = scaler.transform(X_test)
-print("accuracy with scaling: ", knn_scaled.score(X_test_scaled, y_test))
-
-
-# Tune the number of neighbors and contamination
-knn = KNN(n_neighbors=20, contamination=0.01, n_jobs=-1)
-# Let's find outliers in summer of 2020
-knn.fit(X,y)
-probs = knn.predict_proba(algo_df)
-# Use 55% threshold for filtering
-is_outlier = probs[:, 1] > 0.55
-# Isolate the outliers
-outliers = algo_df_summer_2020_for_fit[is_outlier]
-len(outliers)
-
-
-
-
-# Tune the number of neighbors and contamination
-knn = KNN(n_neighbors=20, contamination=0.01, n_jobs=-1)
-# Let's find outliers in summer of 2020
-knn.fit(X)
-probs = knn.predict_proba(X)
-# Use 55% threshold for filtering
-is_outlier = probs[:, 1] > 0.55
-# Isolate the outliers
-outliers = X[is_outlier]
-len(outliers)
-
-# Split the data into features (X) and target (y)
-X = algo_df_summer_2020_for_fit.drop('nitrate_mg_per_liter', axis=1)
-y = algo_df_summer_2020_for_fit['nitrate_mg_per_liter']
-
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# Scale the features using StandardScaler
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(X_train, y_train)
 
 
 
